70ed20e566eacab591527293459aa39c62f85dbb
Chris Gustafson <chris@secondstorydesigns.com>
2015-11-24 11:44:15 -0600

    Statler-Waldorf Marketing Showcase

---
+++ client/app/admin/admin.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('statlerWaldorfAssocApp')
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/admin', {
+        templateUrl: 'app/admin/admin.html',
+        controller: 'AdminCtrl'
+      });
+  });
